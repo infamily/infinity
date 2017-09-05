481ac991b9455a985888565fad2092974b45c518
Christian1992 <christianl1992@outlook.com>
2017-09-05 18:50:28 +0200

    update test setting

--- config/settings/test.py
+++ config/settings/test.py
@@ -59,12 +59,3 @@ TEMPLATES[0]['OPTIONS']['loaders'] = [
         'django.template.loaders.app_directories.Loader',
     ], ],
 ]
-
-REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly'
-    ],
-
-}
