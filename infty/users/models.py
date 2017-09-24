20f25e05900fa02e970b17ea9f9647d7ee2d9908
Mindey <mindey@qq.com>
2017-09-24 00:16:18 +0000

    add User.about field

--- infty/users/models.py
+++ infty/users/models.py
@@ -11,6 +11,7 @@ class User(AbstractUser):
     # First Name and Last Name do not cover name patterns
     # around the globe.
     name = models.CharField(_('Name of User'), blank=True, max_length=255)
+    about = models.TextField(blank=True)
 
     def __str__(self):
         return self.username
