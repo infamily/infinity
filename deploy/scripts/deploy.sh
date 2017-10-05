f2f4563785b464b9767f6e4c963bf1c3e430ce9c
Eugeniy Kuznetsov <atorich@gmail.com>
2017-10-05 16:04:05 +0700

    update deploy script

--- deploy/scripts/deploy.sh
+++ deploy/scripts/deploy.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
-echo "Run ansible playbook..."
-ansible-playbook -vvvv -i deploy/ansible/inventories/staging deploy/ansible/site.yml
+ENVIRONMENT=$1
+PRIVATE_KEY="~/.ssh/${ENVIRONMENT}"
+
+echo "Run ansible playbook for ${ENVIRONMENT} using private key: ${PRIVATE_KEY}..."
+ansible-playbook -vvvv -i deploy/ansible/inventories/staging deploy/ansible/site.yml --private-key=${PRIVATE_KEY}
