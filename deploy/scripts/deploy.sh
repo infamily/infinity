56b407e03f9c1a54143f0db358967d727ce6764e
Eugeniy Kuznetsov <atorich@gmail.com>
2017-10-06 03:39:30 +0700

    build & deploy update

--- deploy/scripts/deploy.sh
+++ deploy/scripts/deploy.sh
@@ -2,5 +2,9 @@
 ENVIRONMENT=$1
 PRIVATE_KEY="~/.ssh/${ENVIRONMENT}"
 
+echo "Push docker image to the Docker Hub..."
+docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
+docker push wefindx/infty:latest
+
 echo "Run ansible playbook for ${ENVIRONMENT} using private key: ${PRIVATE_KEY}..."
 ansible-playbook -vv -i deploy/ansible/inventories/staging deploy/ansible/site.yml --private-key=${PRIVATE_KEY}
