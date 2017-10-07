c4ab71d4984b375e04fc545e0d57e2a0a295250f
Eugeniy Kuznetsov <atorich@gmail.com>
2017-10-07 19:20:46 +0700

    travis merge & push

---
+++ deploy/scripts/git-merge-push.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+cd $HOME
+
+echo "Settings up git..."
+git config --global user.email "info@wefindx.org"
+git config --global user.name "Travis CI"
+
+echo "Clone the repo..."
+git clone --quiet https://${GITHUB_API_KEY}@github.com/${TRAVIS_REPO_SLUG} code
+
+echo "Marking current build..."
+cd ./code
+echo "${TRAVIS_BUILD_NUMBER}" > .buildno
+git add .buildno
+git commit --message "Travis build: ${TRAVIS_BUILD_NUMBER}"
+
+echo "Push to the master..."
+git push --quiet origin master
