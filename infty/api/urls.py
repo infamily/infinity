6b45490417371edef9eab8a6ac24a4ffd6582075
Mindey <mindey@qq.com>
2017-10-31 18:47:04 +0000

    update drf to 3.7.1, add permissions to documentation

--- infty/api/urls.py
+++ infty/api/urls.py
@@ -2,10 +2,16 @@ from django.conf import settings
 from django.conf.urls import include, url
 
 from rest_framework.documentation import include_docs_urls
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 urlpatterns = [
     url(r'^v1/', include('infty.api.v1.urls')),
-    url(r'^v1/docs/', include_docs_urls(title='WeFindX API')),
+    url(r'^v1/docs/',
+        include_docs_urls(
+            title='WeFindX API',
+            permission_classes=(IsAuthenticatedOrReadOnly,)
+        )
+    ),
     url(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework')),
 ]
