9e0a07763ced7eb8788d9b8cd2bcdfcc09a1f118
Ali Aliev <shamkir@gmail.com>
2018-01-04 15:53:54 +0400

    Fixed schema renderer to show the templayte

--- infty/api/v1/views.py
+++ infty/api/v1/views.py
@@ -1,21 +1,22 @@
-from rest_framework.decorators import renderer_classes, api_view, permission_classes
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.views import APIView
+from rest_framework.renderers import CoreJSONRenderer, BrowsableAPIRenderer
 from rest_framework import (
     schemas,
     response,
-    renderers,
 )
 
 schema_generator = schemas.SchemaGenerator(title='Infinity API')
 
 
-@api_view()
-@renderer_classes([renderers.CoreJSONRenderer])
-@permission_classes((IsAuthenticatedOrReadOnly,))
-def schema_view(request):
-    """
-    Explicit schema view
-    http://www.django-rest-framework.org/api-guide/schemas/#using-an-explicit-schema-view
-    """
-    schema = schema_generator.get_schema(request)
-    return response.Response(schema)
+class SchemaView(APIView):
+    renderer_classes = (
+        CoreJSONRenderer,
+        BrowsableAPIRenderer,
+    )
+
+    permission_classes = (IsAuthenticatedOrReadOnly, )
+
+    def get(self, request):
+        schema = schema_generator.get_schema(request)
+        return response.Response(schema)
