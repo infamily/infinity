c36c2dc01de0df3e4e540228ffa40d1f91731f90
Eugeniy Kuznetsov <atorich@gmail.com>
2018-02-07 06:17:42 +0700

    TopicSerializer.categories_str and TopicSerializer.categories_names fields

--- src/api/tests/test_views.py
+++ src/api/tests/test_views.py
@@ -1,14 +1,17 @@
 # coding: utf-8
 from autofixture import AutoFixture
-from rest_framework.reverse import reverse
+from rest_framework.reverse import reverse, reverse_lazy
 from rest_framework.test import APITestCase
 
 from src.core.models import Topic
+from src.meta.models import Type
 from src.users.models import User
 
 
 class TopicCreateTestCase(APITestCase):
     def setUp(self):
+        super(TopicCreateTestCase, self).setUp()
+        AutoFixture(Type, field_values={'is_category': True}).create(3)
         self.user = AutoFixture(User).create(1)[0]
         self.client.force_login(self.user)
 
@@ -33,3 +36,45 @@ class TopicCreateTestCase(APITestCase):
         self.assertEqual(instance.title, '.:en:Test title')
         self.assertEqual(instance.body, '.:en\nTest body')
         self.assertEqual(instance.type, Topic.IDEA)
+
+    def test_create_topic_with_assigned_hyperlinked_categories_created(self):
+        # categories = sorted(Type.objects.categories().values_list('name', flat=True))
+        categories = [reverse('type-detail', args=(o.pk,)) for o in Type.objects.categories().all()]
+
+        self.client.post(reverse('topic-list'), data={
+            'title': '.:en:Test title',
+            'body': '.:en\nTest body',
+            'type': Topic.IDEA,
+            'categories': categories,
+        })
+
+        instance = Topic.objects.first()
+        self.assertEqual(instance.categories.count(), len(categories))
+
+    def test_create_topic_with_assigned_categories_str_created(self):
+        categories_str = sorted(Type.objects.categories().values_list('name', flat=True))
+
+        response = self.client.post(reverse('topic-list'), data={
+            'title': '.:en:Test title',
+            'body': '.:en\nTest body',
+            'type': Topic.IDEA,
+            'categories_str': categories_str,
+        })
+
+        self.assertEqual(categories_str, sorted(response.data['categories_str']))
+
+    def test_create_topic_hyperlinked_and_str_categories_are_merged(self):
+        queryset = Type.objects.categories().order_by('id').all()
+        categories_str = sorted(queryset.values_list('name', flat=True)[0:1])
+        categories_obj = [reverse('type-detail', args=(o.pk,)) for o in queryset[1:]]
+
+        response = self.client.post(reverse('topic-list'), data={
+            'title': '.:en:Test title',
+            'body': '.:en\nTest body',
+            'type': Topic.IDEA,
+            'categories': categories_obj,
+            'categories_str': categories_str,
+        })
+
+        instance = Topic.objects.first()
+        self.assertEqual(instance.categories.count(), len(categories_obj)+len(categories_str))
