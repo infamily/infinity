0e25ba63b836371e7daeda936b938f0cefc580d1
Mindey <mindey@qq.com>
2018-03-05 17:14:55 +0000

    add pk, url to ForeignKey to Payment, serializers

--- src/api/v1/trade/serializers.py
+++ src/api/v1/trade/serializers.py
@@ -6,10 +6,10 @@ from src.trade.models import Payment, Reserve
 class PaymentSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Payment
-        fields = ['request', 'response', 'platform', 'provider']
+        fields = ['id', 'url', 'request', 'response', 'platform', 'provider']
 
 
 class ReserveListSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Reserve
-        fields = ['hours', 'payment', 'transaction', 'currency', 'amount', 'user']
+        fields = ['id', 'url', 'hours', 'payment', 'transaction', 'currency', 'amount', 'user']
